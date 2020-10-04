f0c04c0d46eb88e35c2139cc07cd8139527ae7b1
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-10-04 06:47:21 +0530

    Added bottom bar

--- Assignment1_BMI/Home.js
+++ Assignment1_BMI/Home.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-paper/lib/typescript/src/components/Avatar/Avatar';
+import { createBottomTabNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
-export default class Home extends Component {
+import Metric from './Metric';
+import Standard from './Standard';
+
+class Home extends Component {
     render()
     {
         return(
@@ -12,3 +18,42 @@ export default class Home extends Component {
         );
     }
 }
+
+const BtTab=createMaterialBottomTabNavigator(
+    {
+        Home:{screen:Home, navigationOptions: {
+            tabBarLabel: 'Home',
+            activeColor: '#ff0000',
+            inactiveColor: '#000000',
+            barStyle: {backgroundColor: '#67baf6'},
+            tabBarIcon:()=>(
+                <View>
+                    <Icon name={'home'} size={25} style={{color: '#ff0000'}}/>
+                </View>
+            )}
+        },
+        Standard:{screen:Standard, navigationOptions: {
+            tabBarLabel: 'Standard Meassurement',
+            activeColor: '#ff0000',
+            inactiveColor: '#000000',
+            barStyle: {backgroundColor: '#67baf6'},
+            tabBarIcon:()=>(
+                <View>
+                    <Icon name={'standard measurement'} size={25} style={{color: '#ff0000'}}/>
+                </View>
+            )}
+        },
+        Metric:{screen:Metric, navigationOptions: {
+            tabBarLabel: 'Metric Measurement',
+            activeColor: '#ff0000',
+            inactiveColor: '#000000',
+            barStyle: {backgroundColor: '#67baf6'},
+            tabBarIcon:()=>(
+                <View>
+                    <Icon name={'Metric measurement'} size={25} style={{color: '#ff0000'}}/>
+                </View>
+            )}
+        }
+    }
+);
+export default createAppContainer(BtTab);
