ed0a6d891e42786f7922bc577c394f364f4b2e7f
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-10-22 21:41:55 +0530

    Splash screen working and finalized

--- StormBreaker_Group4_m2/Splash_Screen.js
+++ StormBreaker_Group4_m2/Splash_Screen.js
@@ -1,66 +1,80 @@
 // Import the splash screen requirement classes
 import React, {Component} from 'react';
-import {View, Image, ImageBackground} from 'react-native';
+import {View,ImageBackground, Image, ActivityIndicator, Text} from 'react-native';
 
 // Declare soem global variables for storing the location of the background image:
 var background = require('./background.png');
-var logo_icon = require('./712_logo.png')
+var logo_icon = require('./712_logo.png');
 
 // Declare the main class for the splash screen:
-export default class Splash extends Component{
+export default class Splash_Screen extends Component{
     
     // Initialize render:
-    render(){
-
+    render()
+    {
         // Add return function to display the backgroud and logo:
         return(
             // Initialize the ImageBackground Component:
-        <ImageBackground
-        // Add info about the background picture:
-            source = 
-            {
+                <ImageBackground
+                // Add info about the background picture:
+                source = 
                 {
                     background
                 }
-            }
-            style = 
-            {
-                {
-                    height: '100%', 
-                    width: '100%'
-                }
-            }>
-                {/* Declare View Component overlaying the ImageBackground */}
-                <View
-                // Add styling for the View Component:
                 style = 
                 {
                     {
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center'
+                        height: '100%', 
+                        width: '100%'
                     }
                 }>
-
-                    {/* Declare the Image Component overlaying the View controller for the logo placement */}
-                    <Image 
-                    
-                    // Add info about the logo
-                    source = 
-                    {
-                        {
-                            logo_icon
-                        }
-                    }
+                    {/* Declare View Component overlaying the ImageBackground */}
+                    <View
+                    // Add styling for the View Component:
                     style = 
                     {
                         {
-                            height: '100%',
-                            width: '100%'
+                            flex: 1,
+                            alignItems: 'center',
+                            justifyContent: 'center'
                         }
-                    }></Image>
-                </View> 
-        </ImageBackground>
+                    }>
+                        {/* Declare the Image Component overlaying the View controller for the logo placement */}
+                        <Image 
+                        // Add info about the logo
+                        source = 
+                        {
+                            logo_icon
+                        }
+                        style = 
+                        {
+                            {
+                                height: '80%',
+                                width: '80%'
+                            }
+                        }></Image>
+                        
+                        {/* Declare an ActivityIndicator below the Logo */}
+                        <ActivityIndicator
+                        // Add info about the spinner:
+                        size = "large"
+                        color = "black"
+                        animating = 'true'
+                        >
+                        </ActivityIndicator>
+                        <Text
+                        style = 
+                        {
+                            {
+                                flex: 1,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                fontSize: 25,
+                                padding: 15
+                            }
+                        }>Loading...</Text>
+                    </View> 
+            </ImageBackground>
         );
     }
 }
