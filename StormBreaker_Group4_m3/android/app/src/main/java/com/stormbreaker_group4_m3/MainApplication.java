81e97eec84bcd1c4c1d7772ac9c9327fbfb43523
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-11-13 03:09:35 +0530

    changelist 3

--- StormBreaker_Group4_m3/android/app/src/main/java/com/stormbreaker_group4_m3/MainApplication.java
+++ StormBreaker_Group4_m3/android/app/src/main/java/com/stormbreaker_group4_m3/MainApplication.java
@@ -3,21 +3,18 @@ package com.stormbreaker_group4_m3;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication {
+public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new NavigationReactNativeHost(this) {
+      new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -46,7 +43,7 @@ public class MainApplication extends NavigationApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    
+    SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
